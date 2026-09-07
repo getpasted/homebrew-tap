f4d9af404c2c8e7302475743dbef8e871386897b
John James Jacoby <johnjamesjacoby@me.com>
2026-09-06 21:53:59 -0500

    Homebrew: Remove deprecated URL verification parameter.

--- Casks/pasted.rb
+++ Casks/pasted.rb
@@ -2,8 +2,7 @@ cask "pasted" do
   version "1.0.1"
   sha256 "9b0145fa5796dd6dfc3ee464865639c55440d8bb3a214552a562ddd9a591b455"
 
-  url "https://github.com/getpasted/pasted/releases/download/v#{version}/Pasted_#{version}_universal.dmg",
-      verified: "github.com/getpasted/pasted/"
+  url "https://github.com/getpasted/pasted/releases/download/v#{version}/Pasted_#{version}_universal.dmg"
   name "Pasted"
   desc "Clipboard history, organization, and transformations"
   homepage "https://github.com/getpasted/pasted"
